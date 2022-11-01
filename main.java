b426c39c8b0795e37650c17b0227904a979ae8a6
Pratham Chauhan <Prathamchauhan2002@gmail.com>
2022-11-01 22:16:01 +0530

    final changes done

--- main.java
+++ main.java
@@ -1,42 +1,69 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-class main{
-    public static void main(String[] args){
-    home hm = new home();
+public class main {
+
+    public static void main(String[] args) {
+        GameBoard ttt = new GameBoard();
+        ttt.displayBoard();
+
+        int turn = 1;
+
+        while (ttt.gameStatus() && turn < 10) {
+            if (turn % 2 == 0)
+                ttt.askPlayer('O');
+            else
+                ttt.askPlayer('X');
+            turn++;
+
+            System.out.print("\033[H\033[2J");  
+            System.out.flush();
+            ttt.displayBoard();
+            ttt.checkForWinner();
+
+            if (turn == 10)
+                System.out.print("TIE!\n");
+        }
+
     }
+
 }
 
-public class board {
-    public static char[][] grid = new char[3][3]; // grid to make the board
-    // a nested class init to fill the grid with position numbers 
-       board(){
-        //public static char[][] grid = new char[3][3];
+public class GameBoard {
+    private char[][] gameBoard;
+    private boolean gameOnGoing = true;
+
+    //constructor of GameBoard.
+    public GameBoard() {
+        gameBoard = new char[3][3];
+
         int Iterator=1;
-       for (int i = 0; i<3; i++) {
+        for (int i = 0; i<3; i++) {
          for (int j = 0; j<3; j++){
             char val=Integer.toString(Iterator).charAt(0);
-            grid[i][j]=val;
+            gameBoard[i][j]=val;
             Iterator++;
         }
        }
-       }
-    
-    
-     public static void builder(){
-        for (int row = 0; row < grid.length; row++)
+    }
+
+
+    //To display gameBoard .
+    public void displayBoard() {
+          for (int row = 0; row < gameBoard.length; row++)
         {   
             for (int column = 0; column < 3; column++)
             {   
                 switch (column)
                 {
                     case 0:
-                        System.out.print(" " + grid[row][column] + " | ");
+                        System.out.print(" " + gameBoard[row][column] + " | ");
                         break;
                     case 1:
-                        System.out.print(grid[row][column] + " | ");
+                        System.out.print(gameBoard[row][column] + " | ");
                         break;
                     case 2:
-                        System.out.println(grid[row][column]);
+                        System.out.println(gameBoard[row][column]);
                         break;
                 }
             }
@@ -46,113 +73,92 @@ public class board {
             }
         }
     }
-}
 
-class GameScreen {
-    
-    GameScreen(){ 
-        board brd = new board();
-    //public static boolean gameStatus = false;
-     Scanner scan = new Scanner(System.in);
-    while(true){
+    //to return game status.
+    public boolean gameStatus() {
+        return gameOnGoing;
+    } 
+
+    //take user input and makeMove
+    public void askPlayer(char player) {
+        Scanner keyboard = new Scanner(System.in);
+        int n;
        
-       for(int turn = 1;turn<=9;turn++){
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();
-        brd.builder();
-        if (turn % 2 == 0)
-            { 
-                System.out.print("\nENTER THE POSITION (PLAYER 2) : ");
-                int ch = scan.nextInt();  
-                switch (ch){
-                    case 1:
-                    brd.grid[0][0] = 'X';
-                    break;
-                    case 2:
-                    brd.grid[0][1] = 'X';
-                    break;
-                    case 3:
-                    brd.grid[0][2] = 'X';
-                    break;
-                    case 4:
-                    brd.grid[1][0] = 'X';
-                    break;
-                    case 5:
-                    brd.grid[1][1] = 'X';
-                    break;
-                    case 6:
-                    brd.grid[1][2] = 'X';
-                    break;
-                    case 7:
-                    brd.grid[2][0] = 'X';
-                    break;
-                    case 8:
-                    brd.grid[2][1] = 'X';
-                    break;
-                    case 9:
-                    brd.grid[2][2] = 'X';
-                    break;
-                }
+            System.out.printf("Player %s, please enter the position: ",player);
+            n = keyboard.nextInt();
+      
+    
+
+        makeMove(player,n);
+
+    }  
+
+    public void checkForWinner() {
+        //check over row 
+        for (int row =0; row < gameBoard.length; row++) {
+            if (gameBoard[row][0] == gameBoard[row][1] && gameBoard[row][1] == gameBoard[row][2]
+                    && gameBoard[row][0] != ' ') {
+                System.out.print("The winner is " + gameBoard[row][0] + "!");
+                gameOnGoing = false;
             }
-            else
-            {   System.out.print("\nENTER THE POSITION (PLAYER 1) : ");
-                int ch = scan.nextInt();  
-                 switch (ch){
-                    case 1:
-                    brd.grid[0][0] = 'O';
-                    break;
-                    case 2:
-                    brd.grid[0][1] = 'O';
-                    break;
-                    case 3:
-                    brd.grid[0][2] = 'O';
-                    break;
-                    case 4:
-                    brd.grid[1][0] = 'O';
-                    break;
-                    case 5:
-                    brd.grid[1][1] = 'O';
-                    break;
-                    case 6:
-                    brd.grid[1][2] = 'O';
-                    break;
-                    case 7:
-                    brd.grid[2][0] = 'O';
-                    break;
-                    case 8:
-                    brd.grid[2][1] = 'O';
-                    break;
-                    case 9:
-                    brd.grid[2][2] = 'O';
-                    break;
-                }
 
-            }
-       }
-    }
-    }
-}
-
-class home{
-    home(){
-
-        System.out.println("\033[0;1m" + "TIC-TAC-TOE");
-        System.out.println("1 : Play Game");
-        System.out.println("2 : HighScore");
-		System.out.println("3 : Settings");
-	    
-		System.out.println("Enter your choice::");
-        Scanner scan = new Scanner(System.in);
-        int ch = scan.nextInt();
-        switch(ch){
-            case 1:
-            GameScreen game = new GameScreen();
-            break;
-            case 2:
-            break;
-            case 3:
-            break;
         }
-}
-}
 
+        //check over column 
+        for (int col=0; col < gameBoard[0].length; col++) {
+            if (gameBoard[0][col] == gameBoard[1][col] && gameBoard[1][col] == gameBoard[2][col]
+                    && gameBoard[0][col] != ' ') {
+                System.out.print("The winner is " + gameBoard[0][col] + "!");
+                gameOnGoing = false;
+            }
+
+        }
+
+        //check over diagonals
+        if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2]
+                && gameBoard[0][0] != ' ') {
+            System.out.print("The winner is " + gameBoard[0][0] + "!");
+            gameOnGoing = false;
+        }
+
+        if (gameBoard[2][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[0][2]
+                && gameBoard[2][0] != ' ') {
+            System.out.print("The winner is " + gameBoard[2][0] + "!");
+            gameOnGoing = false;
+        }
+    }
+
+    public boolean makeMove(char player, int n) {
+               switch (n){
+                    case 1:
+                    gameBoard[0][0] = player;
+                    break;
+                    case 2:
+                    gameBoard[0][1] = player;
+                    break;
+                    case 3:
+                    gameBoard[0][2] = player;
+                    break;
+                    case 4:
+                    gameBoard[1][0] = player;
+                    break;
+                    case 5:
+                    gameBoard[1][1] = player;
+                    break;
+                    case 6:
+                    gameBoard[1][2] = player;
+                    break;
+                    case 7:
+                    gameBoard[2][0] = player;
+                    break;
+                    case 8:
+                    gameBoard[2][1] = player;
+                    break;
+                    case 9:
+                    gameBoard[2][2] = player;
+                    break;
+                }    
+                return true;
+    
+    } 
+}
